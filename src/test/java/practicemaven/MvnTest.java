5509e7defa1ed568586b7878f533068a7bf1d58b
Priyanka Ghosh <Priyanka Ghosh@Priyanka>
2023-05-31 21:10:13 +0530

    commit1

---
+++ src/test/java/practicemaven/MvnTest.java
@@ -0,0 +1,17 @@
+package practicemaven;
+
+import org.testng.annotations.Test;
+
+public class MvnTest {
+	@Test
+	public void m1()
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void m2()
+	{
+		System.out.println("how are you");
+	}
+
+}
---
+++ src/test/java/praticemavenng/MvnTestng.java
@@ -0,0 +1,17 @@
+package praticemavenng;
+
+import org.testng.annotations.Test;
+
+public class MvnTestng {
+	@Test
+	public void test1()
+	{
+		System.out.println("Bye");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("see you");
+	}
+
+}
